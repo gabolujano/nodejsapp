d035ef19c42da74585b9f62437b549476ed59c9d
gabolujano <107277090+gabolujano@users.noreply.github.com>
2023-03-31 12:41:29 -0600

    Create nodejsDockerglsDSL.groovy

---
+++ DSL/nodejsDockerglsDSL.groovy
@@ -0,0 +1,25 @@
+job('Aplicacion Node.js Docker DSL') {
+    description('Aplicación Node JS Docker DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/gabolujano/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('gabolujano')
+            node / gitConfigEmail('gabolujano@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('macloujulian/nodejsapp')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
